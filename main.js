9b1cf24cd1f068349034324b6b6740e05f4b96ba
unknown <Bri-dgetCosmus>
2022-06-25 21:13:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+  }
+  function del()             
+  { Current = "0";
+    Operation = 0;                
+    Memory = "0";                  
+    document.Calculator.Display.value = Current;
+    count= calcNumbers < 1
+  }
+  function C()                
+ { Current = "0";
+   document.Calculator.Display.value = Current;
+ }
